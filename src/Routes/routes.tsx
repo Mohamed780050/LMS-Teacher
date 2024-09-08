4c65e03208fb649162b51ecd079ec987fa00d751
Mohamed780050 <Mohamed39omer@gmail.com>
2024-09-08 14:59:24 +0300

    Started commit project from scratch

---
+++ src/Routes/routes.tsx
@@ -0,0 +1,9 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+const routes = createBrowserRouter(createRoutesFromElements(<>
+<Route path="/"></Route>
+</>));
+export default routes;
